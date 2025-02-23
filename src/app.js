8383c399fed6ddcda8f73d3faa7ba23f54052f19
Fariha Promi <farihapromi2611@gmail.com>
2025-02-23 19:11:01 +0600

    connect top mongoDb

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const { productRouter, userRouter } = require('./router');
 const { configureRouter } = require('./router');
 const { logRequestMiddleware, errorHandler } = require('./middlewares');
 
 const app = express();
 const port = 5000;
+const MONGO_URI = process.env.MONGO_URI;
+const MONGO_DB_NAME = process.env.MONGO_DB;
+
+const connectDB = () => {
+  console.log('Connecting mongodb..');
+  mongoose
+    .connect(MONGO_URI, {
+      dbName: MONGO_DB_NAME,
+    })
+    .then(() => {
+      console.log('Mongodb connected successfully');
+    })
+    .catch((err) => console.log(err));
+};
+connectDB();
+
 app.use(express.json()); // Enables parsing of JSON request bodies
 
 app.get('/', (req, res) => {
